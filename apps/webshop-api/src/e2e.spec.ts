771ddf33af9ca7c169d66ec25cdb323748f35f80
Thimo Luijsterburg <Thimo.luijsterburg@rietveld.nl>
2023-04-08 21:57:53 +0200

    Error 4

--- apps/webshop-api/src/e2e.spec.ts
+++ apps/webshop-api/src/e2e.spec.ts
@@ -11,11 +11,12 @@ import {AuthModule} from './app/auth/auth.module';
 
 import {UsersModule} from "./app/users/users.module";
 import {OrderModule} from "./app/order/order.module";
-import {Neo4jModule} from "nest-neo4j/dist";
+import {Neo4jModule, Neo4jScheme} from "nest-neo4j/dist";
 import {ApiResponseInterceptor} from "./app/interceptors/api-response.interceptor";
 import {ManufacturerModule} from "./app/manufacturer/manufacturer.module";
 import {AppController} from "./app/app.controller";
 import {AppService} from "./app/app.service";
+import {environment} from "./environments/environment";
 
 let mongod: MongoMemoryServer;
 let uri: string;
@@ -32,12 +33,11 @@ let uri: string;
     Neo4jModule.forRootAsync({
       useFactory: async () => {
         return {
-          scheme: 'neo4j',
-          host: 'localhost',
-          port: 7687,
-          username: 'neo4j',
-          password: 'password!',
-          database: 'neo4jtest',
+          scheme: environment.NEO4J_URI as Neo4jScheme,
+          host: environment.NEO4J_HOST,
+          port: environment.NEO4J_PORT,
+          username: environment.NEO4J_USERNAME,
+          password: environment.NEO4J_PASSWORD,
         };
       },
     }),
@@ -93,36 +93,36 @@ describe('(e2e) Webshop API', () => {
     expect(app).toBeDefined();
   });
 
-  // it('should create a user', async () => {
-  //   const register = await request(server)
-  //     .get('/api/init');
-  //
-  //   expect(register.body.info.status).toBe(200);
-  //   expect(register.body.info.message).toEqual('OK');
-  // });
-  //
-  // it('should login a user', async () => {
-  //   const login = await request(server)
-  //     .post('/api/auth/register')
-  //     .send({
-  //       email: 'example@gmail.com',
-  //       password: 'PassWord123',
-  //     });
-  //
-  //   expect(login.body.info.status).toBe(201);
-  //   expect(login.body.info.message).toEqual('OK');
-  //   expect(login.body.result.access_token).toBeDefined();
-  // });
-  //
-  // it('should not login a user with wrong password', async () => {
-  //   const response = await request(server)
-  //     .post('/api/auth/login')
-  //     .send({
-  //       "username": "example@gmail.com",
-  //       "password": "PassWord123!"
-  //     });
-  //
-  //   expect(response.body.statusCode).toBe(401);
-  //   expect(response.body.message).toEqual('Unauthorized');
-  // });
+  it('should create a user', async () => {
+    const register = await request(server)
+      .get('/api/init');
+
+    expect(register.body.info.status).toBe(200);
+    expect(register.body.info.message).toEqual('OK');
+  });
+
+  it('should login a user', async () => {
+    const login = await request(server)
+      .post('/api/auth/register')
+      .send({
+        email: 'example@gmail.com',
+        password: 'PassWord123',
+      });
+
+    expect(login.body.info.status).toBe(201);
+    expect(login.body.info.message).toEqual('OK');
+    expect(login.body.result.access_token).toBeDefined();
+  });
+
+  it('should not login a user with wrong password', async () => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({
+        "username": "example@gmail.com",
+        "password": "PassWord123!"
+      });
+
+    expect(response.body.statusCode).toBe(401);
+    expect(response.body.message).toEqual('Unauthorized');
+  });
 });
