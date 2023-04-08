4e7bdcb6452f2c64c307f15be56f622b29f6383f
Thimo Luijsterburg <Thimo.luijsterburg@rietveld.nl>
2023-04-08 18:17:48 +0200

    NEO4J online environment done

--- apps/webshop-api/src/environments/environment.prod.ts
+++ apps/webshop-api/src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
 
-  DB_CONNECTION_STRING: 'mongodb+srv://admin:PassWord123@mono-webshop-cluster.e1hdvea.mongodb.net/test'
+  DB_CONNECTION_STRING: 'mongodb+srv://admin:PassWord123@mono-webshop-cluster.e1hdvea.mongodb.net/test',
+
+  NEO4J_USERNAME: 'neo4j',
+  NEO4J_PASSWORD: 'GCPKalbQHeNOCQgUVbJk76HU3eVyXiDyqAoQuwfjbS8',
+  NEO4J_URI: 'neo4j+s://617daffd.databases.neo4j.io',
+  NEO4J_HOST: '617daffd.databases.neo4j.io',
+  NEO4J_PORT: 7687,
 };
