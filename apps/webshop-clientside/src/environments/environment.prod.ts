5f81f7aa8409c9e4d1ddaea4b7a3e9e411f674d0
Thimo Luijsterburg <Thimo.luijsterburg@rietveld.nl>
2023-04-08 21:21:48 +0200

    final commit

--- apps/webshop-clientside/src/environments/environment.prod.ts
+++ apps/webshop-clientside/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
 
   SERVER_API_URL: 'https://monowebshop-production.up.railway.app/api/',
 
-  APP_NAME: 'Webshop',
+  APP_NAME: 'Mono-Webshop',
   APP_VERSION: '1.0.0',
   APP_DESCRIPTION: 'Webshop in MonoRepo with Nx',
   APP_AUTHOR: 'Thimo Luijsterburg - 2184240',
